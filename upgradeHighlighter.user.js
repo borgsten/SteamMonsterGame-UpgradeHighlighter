ac5cbeeed0b4785fd288c3b5a8bcbabe0e266a5b
Eric Borgsten <eric.borgsten@gmail.com>
2015-06-16 00:30:14 +0200

    Version 1.1. Fixed bug where i iterated over the wrong variable.

--- upgradeHighlighter.user.js
+++ upgradeHighlighter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SteamMonsterGame-UpgradeHighlighter
 // @namespace    https://github.com/borgsten/SteamMonsterGame-UpgradeHighlighter
-// @version      1.0
+// @version      1.1
 // @description  Calculates which upgrade is the most cost effective and highlights it
 // @grant none
 // @match        http://steamcommunity.com/minigame/towerattack/
@@ -16,9 +16,8 @@
     var cheapest    = "#00d000"; //Green
     var expensive   = "#ff0000"; //Red
 
-    var printQuota  = false;
-
-
+    var debug  = false;
+    var clientSide  = true;
 
 
     var prop = [{"Delta":1300,"Group":0},
@@ -46,15 +45,15 @@
                 {"Delta":1000,"Group":2},
                 {"Delta":1000000,"Group":0},
                 {"Delta":10000,"Group":1},
-                {"Delta":10000,"Group":2}];
+                {"Delta":10000,"Group":2},
+                {"Delta":10000000,"Group":0},
+                {"Delta":100000,"Group":1},
+                {"Delta":100000,"Group":2}];
 
     var bestArmor, bestDPS, bestDMG;
 
-    function s() {
-        return g_Minigame.m_CurrentScene;
-    };
     // Game started function
-    function gameRunning() {
+    function gameReady() {
         try {
             var ready = document.querySelector("div.container_upgrades").children.length;
             return (typeof g_Minigame === "object" && g_Minigame.m_CurrentScene.m_rgGameData.status == 2 && ready != 0);
@@ -66,28 +65,39 @@
 
     function MainLoop(){
 
-        updateQuotas();
+        if(clientSide){
+            getOptimalUpgrade();
 
-        var buttons = document.querySelector("div.container_upgrades").getElementsByClassName("link");
+            var buttons = document.querySelector("div.container_upgrades").getElementsByClassName("link");
 
-        for(i=0;i<buttons.length;i++){
-            buttons[i].addEventListener("click", updateQuotas);   
+            for(i=0;i<buttons.length;i++){
+                buttons[i].addEventListener("click", getOptimalUpgrade);   
+            }
         }
-
+        else
+            setInterval(function(){getOptimalUpgrade();},1000);
     };
-    function updateQuotas(){
+    
+    function getOptimalUpgrade(){
         bestArmor = [Infinity,-1];
         bestDPS = [Infinity,-1];
         bestDMG = [Infinity,-1];
 
-        updateList();
+        if(clientSide)
+            updateListClientSide();
+        else
+            updateListServerSide();
+
         var div_container = document.querySelector("div.container_upgrades").children;
 
         for(i=0;i<div_container.length;i++){
-            var id = parseId(div_container[i].getAttribute('id'));
+            
+            var id = parseInt(div_container[i].getAttribute('id'));
             var elem = div_container[i].getElementsByClassName("upgrade_text")[0];
-            if(printQuota)
+            
+            if(debug && prop[id].PricePerUnit != undefined)
                 elem.innerHTML=prop[id].PricePerUnit.toExponential(5);
+            
             if(bestArmor[1] == id || bestDPS[1] == id || bestDMG[1] == id)
                 elem.style.color = cheapest;
             else
@@ -95,49 +105,77 @@
         }
     };
 
-    function parseId(id){
-        return id.replace(/\D/g,'');
+    function parseInt(s){
+        return s.replace(/\D/g,'');
     };
 
+    function parsePrefix(s){
+        var prefix = {"" : 1, "K" : 1000, "M" : 1000000, "B" : 1000000000, "T" : 1000000000000};
+        return prefix[s.replace(/[\W\d]+/g,'')];        
+    };
 
-    function updateList(){
-        var upgrades = g_Minigame.m_CurrentScene.m_rgPlayerUpgrades;
-        for(i = 0; i<prop.length;i++){
-            if(prop[i].Group == null)
+    function updateListClientSide(){
+        var div_container = document.querySelector("div.container_upgrades").children;
+
+
+        for(i=0;i<div_container.length;i++){
+            var id = parseInt(div_container[i].getAttribute('id'));
+            var string = div_container[i].querySelector("div.cost").innerHTML;
+            var cost = parseInt(string)*parsePrefix(string);
+
+            if(prop[id].Group == null)
                 continue;
-            prop[i].PricePerUnit=(upgrades[i].cost_for_next_level)/prop[i].Delta;   
 
-            switch(prop[i].Group){
-                case 0:
-                    if(prop[i].PricePerUnit < bestArmor[0]){
-                        bestArmor[0] = prop[i].PricePerUnit;
-                        bestArmor[1] = i;
-                    }
-                    break;
-                case 1:
-                    if(prop[i].PricePerUnit < bestDPS[0]){
-                        bestDPS[0] = prop[i].PricePerUnit;
-                        bestDPS[1] = i;
-                    }
-                    break;
-                case 2:
-                    if(prop[i].PricePerUnit < bestDMG[0]){
-                        bestDMG[0] = prop[i].PricePerUnit;
-                        bestDMG[1] = i;
-                    }
-                    break;
-            };
+            prop[id].PricePerUnit=cost/prop[id].Delta;
+
+            updateBest(id);
+        }
+    };
+    
+    function updateListServerSide(){
+        var upgrades = g_Minigame.m_CurrentScene.m_rgPlayerUpgrades;
+        console.log(upgrades);
+        for(i = 0; i<upgrades.length;i++){
+            var id = upgrades[i].upgrade;
+
+            if(prop[id].Group == null)
+                continue;
+
+            prop[id].PricePerUnit=upgrades[i].cost_for_next_level/prop[id].Delta;
+
+            updateBest(id);
         };
     };
-
-    var startAll = setInterval(function() { 
-        if(!gameRunning())
+    
+    function updateBest(id){
+        switch(prop[id].Group){
+            case 0:
+                if(prop[id].PricePerUnit < bestArmor[0]){
+                    bestArmor[0] = prop[id].PricePerUnit;
+                    bestArmor[1] = id;
+                }
+                break;
+            case 1:
+                if(prop[id].PricePerUnit < bestDPS[0]){
+                    bestDPS[0] = prop[id].PricePerUnit;
+                    bestDPS[1] = id;
+                }
+                break;
+            case 2:
+                if(prop[id].PricePerUnit < bestDMG[0]){
+                    bestDMG[0] = prop[id].PricePerUnit;
+                    bestDMG[1] = id;
+                }
+                break;
+        };
+    };
+    
+    var start = setInterval(function() { 
+        if(!gameReady())
             return;
 
-        clearInterval(startAll);				
+        clearInterval(start);               
         MainLoop();
     }, 1000);
-
-
 
 }(window));
